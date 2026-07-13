281c438e0f0d706c8f527c805dc938259f5b2b56
JerryPapada <gerasimospapd@gmail.com>
2026-07-13 13:25:38 +0300

    minor fixes

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import serializers
 
-
 @extend_schema(
     tags=["core"],
     summary="Health check",
